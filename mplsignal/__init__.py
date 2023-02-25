a3785f43a6501652d9ffcefd24da3a14510b5278
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2023-02-25 20:55:11 +0100

    Add functions to __init__

--- mplsignal/__init__.py
+++ mplsignal/__init__.py
@@ -8,3 +8,7 @@ at creating signal processing-related plots.
 # Must import __version__ first to avoid errors importing this file during the build process.
 # See https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
 from ._version import __version__
+from .freq_plots import freqz, freqz_fir, freqz_tf, freqz_zpk
+from .plane_plots import zplane, zplane_tf
+
+__all__ = ['freqz', 'freqz_fir', 'freqz_tf', 'freqz_zpk', 'zplane', 'zplane_tf']
