2f9873ee1c5df0351912676ca16e2b5953e445ca
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2023-02-26 19:01:45 +0100

    Enable configuring multiplicity text properties

--- mplsignal/tests/test_plane_plots.py
+++ mplsignal/tests/test_plane_plots.py
@@ -18,3 +18,14 @@ def test_zplane_multiple():
     zeros = [0.5 + 0.5j, 0.5 - 0.5j] * 3
     poles = [-0.5 + 0.5j, -0.5 - 0.5j] * 2
     plane_plots.zplane(zeros, poles, ax=ax)
+
+
+@image_comparison(
+    ['zplane_multiplicity_properties.png'], remove_text=True, style="mpl20"
+)
+def test_zplane_multiplicity_properties():
+    fig, ax = plt.subplots()
+    zeros = [0.2 + 0.8j, 0.2 - 0.8j] * 3
+    poles = [-0.7 + 0.3j, -0.7 - 0.3j] * 2
+    props = {'color': 'b', 'fontsize': 20, 'backgroundcolor': 'g', 'rotation': 45}
+    plane_plots.zplane(zeros, poles, ax=ax, multiplicity_props=props)
